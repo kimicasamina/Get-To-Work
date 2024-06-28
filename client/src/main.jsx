eab97ee3cac8e9718e84b5fcd9c52289e8c22b93
kimi.casamina <kimi.casamina@gmail.com>
2024-06-28 15:59:39 +0800

    changed axios baseURL

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,7 +9,7 @@ import store from "./redux/store.js";
 
 // axios imports and config
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:4000/api";
+axios.defaults.baseURL = "https://gettowork.onrender.com/api";
 axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
