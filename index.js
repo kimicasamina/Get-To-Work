63d80f8fd360d2f03a7d3bfd3d0b66e6b394c278
kimi.casamina <kimi.casamina@gmail.com>
2024-12-19 18:01:54 +0800

    fix: serving static files

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
-  );
+  )
 } else {
   app.get("/", (req, res) => res.send("Server is ready..."));
 }
